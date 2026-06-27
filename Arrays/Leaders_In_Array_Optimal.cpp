626ee2684a20cc0252cefeb343197b3fd82bdd71
vanshika <vk340649@gmail.com>
2026-06-27 22:19:40 +0530

    Leaders in array Completed

--- Arrays/Leaders_In_Array_Optimal.cpp
+++ Arrays/Leaders_In_Array_Optimal.cpp
@@ -2,7 +2,27 @@
 using namespace std;
 class Solution{
     public:
-    vector<int> leaders(vector<int>&arr,int n){
-        
+    vector<int> leaders(){
+        vector<int> arr={10,22,12,3,0,6};
+        int n=arr.size();
+        int maxi=INT_MIN;
+        vector<int> ans;
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]>maxi){
+                ans.push_back(arr[i]);
+            }
+            maxi=max(maxi,arr[i]);
+            
+        }
+        sort(ans.begin(),ans.end());
+        for(auto it:ans){
+            cout<<it<<" ";
+        }
+        return ans;
     }
+};
+int main(){
+    Solution o;
+    o.leaders();
+    return 0;
 }
